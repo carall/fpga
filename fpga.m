4b186ec06d7030e46ca71b2a7b08f83a8cb14926
wys <yunsong_wang@163.com>
2017-01-20 03:37:00 +0800

    update with the Lab code and rename functions

--- fpga.m
+++ fpga.m
@@ -1,23 +1,23 @@
 function countVal = fpga( time, comName )
 % open serial
-s = OpenSerial( comName );
+s = openSerial( comName );
 % set input delay
 delayInfo = zeros( 2, 16 );
 delayInfo( 1,1 ) = 1;
-SetDelay( s, delayInfo );
+setDelay( s, delayInfo );
 % set counter
 counterInfo = zeros( 3, 64 );
 counterInfo( 2,1 ) = 1;
 counterInfo( 1,: ) = 1;
-SetCounter( s, counterInfo );
+setCounter( s, counterInfo );
 % set count time
-SetTime( s, time );
+setTime( s, time );
 % receiving configure-done signal
 cfgDone = dec2bin(fread(s,1,'uint32'),32);
 assert((str2double(cfgDone(1)) == 1),'Configure failed!');
 disp('configure done!');
 %read data
-[ countVal, countDone ] = ReadCount( s );
+[ countVal, countDone ] = seadCount( s );
 assert((countDone >= 2^31+2^30),'Reading counting done failed!');
 fclose(s);
 end
---
+++ readCount.m
@@ -0,0 +1,30 @@
+function [ countVal,countDone ] = ReadCount( serialName )
+% READCOUNT read count value from FPGA
+%   serialName     serial variable name
+disp('read data...');
+fid = fopen('F:\LabVIEW Data\cnt.txt','a+');
+fprintf(fid, '\n\n\n');
+temp = fread(serialName,1,'uint32');
+cntPrev = 0;
+timer = tic;
+countVal = zeros(1,64);
+while temp<2^31+2^30 %read one counter data in one loop
+    countValLow = dec2bin(temp,32);
+    countValHigh = dec2bin(fread(serialName,1,'uint32'),32);
+    countAddressLow = bin2dec(countValLow(3:8));
+    countAddressHigh = bin2dec(countValHigh(3:8));
+    assert((countAddressLow == countAddressHigh),'counter data address error!');
+    address = countAddressLow;
+    val = bin2dec(countValLow(9:32)) + bin2dec(countValHigh(9:32))*2^24;
+    countVal(address+1) = val;
+    if address == 0
+        fprintf(fid, '%d\t%d\r\n', timer,val);
+        disp(val - cntPrev);
+        cntPrev = val;
+    end
+    temp = fread(serialName,1,'uint32');
+    timer = tic;
+end
+countDone = temp;
+fclose(fid);
+end
